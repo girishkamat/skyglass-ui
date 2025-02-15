b73a6d389131128508d2bfec4a18b1ab992d509d
Girish Kamath <girish.kamat@gmail.com>
2025-02-15 13:05:41 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,223 @@
+import './App.css';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import SearchIcon from '@mui/icons-material/Search';
+import SubtitlesIcon from '@mui/icons-material/Subtitles';
+import AudiotrackIcon from '@mui/icons-material/Audiotrack';
+import AspectRatioIcon from '@mui/icons-material/AspectRatio';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import Popper from '@mui/material/Popper';
+import SubtitleCustomizer from './SubtitleCustomizer'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+  typography: {
+    fontFamily: 'Arial, Helvetica, sans-serif !important',
+  },
+});
+
+
+export default function App() {
+
+  const remoteImg = React.useRef(null);
+
+  const fonts = [
+    "Arial, sans-serif",
+    "Helvetica, sans-serif",
+    "Verdana, sans-serif",
+    "Tahoma, sans-serif",
+    "Verdana, sans-serif",
+    "Roboto, sans-serif",
+    "Open Sans, sans-serif",
+    "Lato, sans-serif",
+    "Times New Roman, serif",
+    "Georgia, serif",
+    "Courier New, monspace"
+  ];
+
+  const sizes = new Map([
+    ["20", "Small"], ["30", "Medium"], ["40", "Large"]
+  ])
+
+  const colors = new Map([
+    ["#FFFFFF", "White"], ["#FFFF00", "Yellow"], ["#CCCCCC", "Light Gray"], ["#00FFFF", "Cyan"], ["#FFA500", "Orange"], ["#FF0000", "Red"]
+  ])
+
+  const backgrounds = new Map([
+    ["transperant", "Transperant"], ["#FFFFFF", "White"], ["#FFFF00", "Yellow"], ["#CCCCCC", "Light Gray"], ["#00FFFF", "Cyan"], ["#FFA500", "Orange"], ["#FF0000", "Red"]
+  ])
+
+  const lineSpacings = new Map([
+    ["1.2", "Low"], ["1.4", "Medium"], ["1.6", "High"]
+  ])
+
+  const [appSettings, setAppSettings] = React.useState({
+    visibleBottomNav: false,
+    bottomNavTabIndex: 0,
+    subtitlePopupAnchorEl: null,
+
+    subtitleSettings: {
+      switch: 'off',
+      language: 'english',
+
+      font: "Arial, sans-serif",
+      size: 20,
+      color: "#FFFFFF",
+      background: "transperant",
+      backgroundHex: "transperant",
+      opacity: 0,
+      lineSpacing: 1.2,
+      position: "bottom",
+      currentProfile: 0,
+      profileName: '',
+
+      profiles: new Map([[0, {
+        profileName: 'Default',
+        font: "Arial, sans-serif",
+        size: 20,
+        color: "#FFFFFF",
+        backgroundHex: "transperant",
+        opacity: 0,
+        lineSpacing: 1.2,
+        position: "bottom"
+      }]]),
+      fonts: fonts,
+      sizes: sizes,
+      colors: colors,
+      backgrounds: backgrounds,
+      lineSpacings: lineSpacings
+    }
+  })
+
+  const subtitlesPopperOpen = Boolean(appSettings.subtitlePopupAnchorEl);
+  const subtitlesPopperId = subtitlesPopperOpen ? 'subtitlesPopper' : undefined;
+
+  React.useEffect(() => {
+    if (appSettings.bottomNavTabIndex === 1) {
+      setAppSettings({ ...appSettings, subtitlePopupAnchorEl: document.getElementById("subtitlesNav") })
+    } else {
+      setAppSettings({ ...appSettings, subtitlePopupAnchorEl: null })
+    }
+  }, [appSettings.bottomNavTabIndex]);
+
+  const handleRemoteClick = (e) => {
+    var offset = remoteImg.current.getBoundingClientRect();
+    var x = Math.floor((e.pageX - offset.left) / offset.width * 10000) / 100;
+    var y = Math.floor((e.pageY - offset.top) / offset.height * 10000) / 100;
+    console.log(x + "," + y)
+    const newAppSettings = {}
+    if (x > 15 && x < 34 && y > 30 && y < 40) {
+      // subtitles button
+      newAppSettings.visibleBottomNav = !appSettings.visibleBottomNav
+      if (!newAppSettings.visibleBottomNav) {
+        newAppSettings.bottomNavTabIndex = 0
+      } else {
+        newAppSettings.bottomNavTabIndex = 1
+      }
+    } else if (x > 52 && x < 83 && y > 42 && y < 72) {
+      // left button
+      const newBottomNavTabIndex = appSettings.bottomNavTabIndex + 1
+      if (newBottomNavTabIndex >= 0 && newBottomNavTabIndex <= 4) {
+        newAppSettings.bottomNavTabIndex = newBottomNavTabIndex
+      }
+    } else if (x > 17 && x < 52 && y > 42 && y < 72) {
+      // right button
+      const newBottomNavTabIndex = appSettings.bottomNavTabIndex - 1
+      if (newBottomNavTabIndex >= 0 && newBottomNavTabIndex <= 4) {
+        newAppSettings.bottomNavTabIndex = newBottomNavTabIndex
+      }
+    } else if (x > 17 && x < 34 && y > 78 && y < 85) {
+      newAppSettings.visibleBottomNav = false
+      newAppSettings.bottomNavTabIndex = 0
+    }
+
+    setAppSettings({ ...appSettings, ...newAppSettings })
+  }
+
+  const subtitleComponent = (language) => {
+    return (
+      <div style={{ display: 'inline-block' }}>
+        <div style={{
+          padding: '5px',
+          lineHeight: appSettings.subtitleSettings.lineSpacing, textAlign: 'center', display: 'block', position: 'relative',
+          fontFamily: appSettings.subtitleSettings.font,
+          fontSize: `${appSettings.subtitleSettings.size}px`,
+          color: appSettings.subtitleSettings.color,
+          backgroundColor: appSettings.subtitleSettings.background
+        }}>
+          {language === "english" ? "This is a preview of subtitles" : "Esta es una vista previa de los subtítulos"}
+        </div>
+        <div style={{
+          padding: '5px',
+          lineHeight: appSettings.subtitleSettings.lineSpacing, textAlign: 'center', display: 'block', position: 'relative',
+          fontFamily: appSettings.subtitleSettings.font,
+          fontSize: `${appSettings.subtitleSettings.size}px`,
+          color: appSettings.subtitleSettings.color,
+          backgroundColor: appSettings.subtitleSettings.background
+        }}>
+          {language === "english" ? "This is another line showing preview of subtitles" : "Esta es otra línea que muestra una vista previa de los subtítulos"}
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <div className="container">
+      <div className="tvContainer">
+        <Box sx={{ maxWidth: '100%', maxHeight: '100%', display: 'inline-block', position: 'relative' }}>
+          <img src='/movie5.jpg' className="movieImage" />
+          <ThemeProvider theme={darkTheme}>
+            <Popper
+              id={subtitlesPopperId}
+              open={subtitlesPopperOpen}
+              anchorEl={appSettings.subtitlePopupAnchorEl}
+              placement='top'
+            >
+              <SubtitleCustomizer appSettings={appSettings} setAppSettings={setAppSettings} />
+            </Popper>
+
+            {appSettings.visibleBottomNav ?
+              <BottomNavigation
+                id="subtitlesNav"
+                showLabels
+                value={appSettings.bottomNavTabIndex}
+                onChange={(event, newValue) => {
+                  setAppSettings({ ...appSettings, bottomNavTabIndex: newValue })
+                }}
+                sx={{ position: 'relative', left: 20, bottom: 80, width: '100%' }}
+              >
+                <BottomNavigationAction label="Search" icon={<SearchIcon />} />
+                <BottomNavigationAction label="Subtitles" icon={<SubtitlesIcon />} />
+                <BottomNavigationAction label="Audio Description" icon={<AudiotrackIcon />} />
+                <BottomNavigationAction label="Viewing Mode" icon={<AspectRatioIcon />} />
+                <BottomNavigationAction label="Night Mode" icon={<DarkModeIcon />} />
+              </BottomNavigation>
+              : ""}
+          </ThemeProvider>
+        </Box>
+        {appSettings.subtitleSettings.switch === "on" && appSettings.visibleBottomNav === false ?
+          <div
+            style={{ width: '1100px', display: 'grid', }}
+          ><div
+            style={{
+              position: 'absolute',
+              bottom: `${appSettings.subtitleSettings.position === "bottom" ? "80" : "700"}px`,
+              left: '50%',
+              transform: 'translateX(-50%)',
+              padding: '8px 16px'
+            }}
+          >
+              {subtitleComponent(appSettings.subtitleSettings.language)}
+            </div></div> : ""}
+      </div>
+      <div className="remoteContainer">
+        <img onClick={handleRemoteClick} ref={remoteImg} src='/sky-remote.png' className="remoteImage" />
+      </div>
+    </div>
+  );
+}
---
+++ src/SubtitleCustomizer.js
@@ -0,0 +1,405 @@
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+import { MenuItem, Select, Slider, Box, TextField, Button } from "@mui/material";
+import Typography from '@mui/material/Typography';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Tooltip from '@mui/material/Tooltip';
+import PropTypes from 'prop-types';
+
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import ListItemButton from '@mui/material/ListItemButton';
+
+function ValueLabelComponent(props) {
+  const { children, value } = props;
+
+  return (
+    <Tooltip enterTouchDelay={0} placement="top" title={value}>
+      {children}
+    </Tooltip>
+  );
+}
+
+ValueLabelComponent.propTypes = {
+  children: PropTypes.element.isRequired,
+  value: PropTypes.node,
+};
+
+export default function SubtitleCustomizer({ appSettings, setAppSettings }) {
+
+
+  function hexToRgb(hex, opacity) {
+    // Remove '#' if present
+    hex = hex.replace(/^#/, '');
+
+    // Parse shorthand hex codes (e.g., #abc -> #aabbcc)
+    if (hex.length === 3) {
+      hex = hex.split('').map(char => char + char).join('');
+    }
+
+    // Convert to RGB values
+    const bigint = parseInt(hex, 16);
+    const r = (bigint >> 16) & 255;
+    const g = (bigint >> 8) & 255;
+    const b = bigint & 255;
+
+    return `rgb(${r}, ${g}, ${b}, ${opacity})`;
+  }
+
+  function updateAppSettingsForSubtitles(updatedSubtitleSettings) {
+    setAppSettings({
+      ...appSettings,
+      ...{
+        subtitleSettings: {
+          ...appSettings.subtitleSettings,
+          ...updatedSubtitleSettings
+        }
+      }
+    })
+  }
+
+  function handleBackground(e) {
+    updateAppSettingsForSubtitles({
+      background: hexToRgb(e.target.value, appSettings.subtitleSettings.opacity / 100),
+      backgroundHex: e.target.value
+    })
+  }
+
+
+  function handleOpacity(newValue) {
+    updateAppSettingsForSubtitles({
+      opacity: newValue,
+      background: appSettings.subtitleSettings.background === "transperant" ? appSettings.subtitleSettings.background : hexToRgb(appSettings.subtitleSettings.backgroundHex, newValue / 100)
+    })
+  }
+
+  const handleLanguageChange = (event) => {
+    updateAppSettingsForSubtitles({
+      language: event.target.value
+    })
+  }
+
+  const handleProfileNameChange = (event) => {
+    updateAppSettingsForSubtitles({
+      profileName: event.target.value
+    })
+  }
+
+  const handlePosition = (event) => {
+    updateAppSettingsForSubtitles({
+      position: event.target.value
+    })
+  };
+
+  const handleProfileChange = (newProfileId) => {
+    updateAppSettingsForSubtitles({
+      currentProfile: newProfileId,
+      font: appSettings.subtitleSettings.profiles.get(newProfileId).font,
+      size: appSettings.subtitleSettings.profiles.get(newProfileId).size,
+      color: appSettings.subtitleSettings.profiles.get(newProfileId).color,
+      background: hexToRgb(appSettings.subtitleSettings.profiles.get(newProfileId).backgroundHex, appSettings.subtitleSettings.profiles.get(newProfileId).opacity / 100),
+      backgroundHex: appSettings.subtitleSettings.profiles.get(newProfileId).backgroundHex,
+      opacity: appSettings.subtitleSettings.profiles.get(newProfileId).opacity,
+      lineSpacing: appSettings.subtitleSettings.profiles.get(newProfileId).lineSpacing,
+      position: appSettings.subtitleSettings.profiles.get(newProfileId).position
+    })
+  };
+
+  const handleSaveProfile = () => {
+    const newCurrentProfile = appSettings.subtitleSettings.profileName
+    const newProfileId = appSettings.subtitleSettings.profiles.size
+    updateAppSettingsForSubtitles({
+      currentProfile: newProfileId, 
+      profileName: '',
+      profiles: new Map([...appSettings.subtitleSettings.profiles, [
+        newProfileId, {
+          profileName: newCurrentProfile,
+          font: appSettings.subtitleSettings.font,
+          size: appSettings.subtitleSettings.size,
+          color: appSettings.subtitleSettings.color,
+          backgroundHex: appSettings.subtitleSettings.backgroundHex,
+          opacity: appSettings.subtitleSettings.opacity,
+          lineSpacing: appSettings.subtitleSettings.lineSpacing,
+          position: appSettings.subtitleSettings.position,
+        }
+      ]])
+    })
+  }
+
+  const handleDeleteProfile = (profileId) => {
+    var newCurrentProfile = appSettings.subtitleSettings.currentProfile
+    if(appSettings.subtitleSettings.currentProfile === profileId) {
+      newCurrentProfile = 0
+    }
+
+    updateAppSettingsForSubtitles({
+      currentProfile: newCurrentProfile,
+      profiles: new Map([...appSettings.subtitleSettings.profiles.entries().filter(([key, value]) => key !== profileId)]),
+      font: appSettings.subtitleSettings.profiles.get(newCurrentProfile).font,
+      size: appSettings.subtitleSettings.profiles.get(newCurrentProfile).size,
+      color: appSettings.subtitleSettings.profiles.get(newCurrentProfile).color,
+      background: hexToRgb(appSettings.subtitleSettings.profiles.get(newCurrentProfile).backgroundHex, appSettings.subtitleSettings.profiles.get(newCurrentProfile).opacity / 100),
+      backgroundHex: appSettings.subtitleSettings.profiles.get(newCurrentProfile).backgroundHex,
+      opacity: appSettings.subtitleSettings.profiles.get(newCurrentProfile).opacity,
+      lineSpacing: appSettings.subtitleSettings.profiles.get(newCurrentProfile).lineSpacing,
+      position: appSettings.subtitleSettings.profiles.get(newCurrentProfile).position
+    })
+  }
+
+  const handleSwitchChange = (event) => {
+    updateAppSettingsForSubtitles({ switch: event.target.value })
+  }
+
+  const handleFontChange = (event) => {
+    updateAppSettingsForSubtitles({ font: event.target.value })
+  }
+
+  const handleFontSizeChange = (e) => {
+    updateAppSettingsForSubtitles({ size: e.target.value })
+  }
+
+  const handleFontColorChange = (e) => updateAppSettingsForSubtitles({ color: e.target.value })
+
+  const handleLineSpacingChange = (e) => updateAppSettingsForSubtitles({ lineSpacing: e.target.value })
+
+
+
+  return (
+    <div style={{
+      width: '1200px', display: 'grid',
+      placeItems: 'center'
+    }}>
+      <div style={{
+        padding: '10px',
+        marginBottom: '10px',
+        fontFamily: appSettings.subtitleSettings.font,
+        fontSize: `${appSettings.subtitleSettings.size}px`,
+        color: appSettings.subtitleSettings.color,
+        backgroundColor: appSettings.subtitleSettings.background
+      }}>
+        {appSettings.subtitleSettings.language === "english" ? <div style={{ display: 'inline-block' }}>
+          <div style={{ lineHeight: appSettings.subtitleSettings.lineSpacing, textAlign: 'center', display: 'block' }}>
+            This is a preview of subtitles
+          </div>
+          <div style={{ lineHeight: appSettings.subtitleSettings.lineSpacing, textAlign: 'center', display: 'block' }}>
+            This is another line showing preview of subtitles
+          </div>
+        </div> : ""}
+        {appSettings.subtitleSettings.language === "spanish" ? <div>
+          <div style={{ lineHeight: appSettings.subtitleSettings.lineSpacing, textAlign: 'center', display: 'block' }}>
+            Esta es una vista previa de los subtítulos.
+          </div>
+          <div style={{ lineHeight: appSettings.subtitleSettings.lineSpacing, textAlign: 'center', display: 'block' }}>
+            Esta es otra línea que muestra una vista previa de los subtítulos.
+          </div>
+        </div> : ""}
+      </div>
+
+      <Stack direction="row" sx={{ bgcolor: 'rgba(0,0,0,0.8)', padding: '20px', borderRadius: '15px', width: '500px'}} spacing={1}>
+
+        <Stack sx={{ padding: '20px', borderRight: 1, borderColor: 'gray' }} spacing={2}>
+
+          <Typography variant="h6" gutterBottom sx={{ color: 'text.primary' }}>
+            <strong>Subtitle Settings</strong>
+          </Typography>
+
+          <FormControl>
+            <InputLabel id="font-label">Font</InputLabel>
+            <Select
+              labelId="font-label"
+              id="font"
+              value={appSettings.subtitleSettings.font}
+              label="Font"
+              onChange={handleFontChange}
+              size="small"
+            >
+              {appSettings.subtitleSettings.fonts.map((font) => (
+                <MenuItem key={font} value={font} style={{ fontFamily: font }}>
+                  {font.split(",")[0]}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+
+          <FormControl>
+            <InputLabel id="fontSize-label">Font Size</InputLabel>
+            <Select
+              labelId="fontSize-label"
+              id="fontSize"
+              value={appSettings.subtitleSettings.size}
+              label="Font Size"
+              onChange={handleFontSizeChange}
+              size="small"
+            >
+              {Array.from(appSettings.subtitleSettings.sizes).map(([key, value]) => (
+                <MenuItem key={key} value={key}>{value}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+
+          <FormControl>
+            <InputLabel id="fontColor-label">Font Color</InputLabel>
+            <Select
+              labelId="fontColor-label"
+              id="fontColor"
+              value={appSettings.subtitleSettings.color}
+              label="Font Color"
+              onChange={handleFontColorChange}
+              size="small"
+            >
+              {Array.from(appSettings.subtitleSettings.colors).map(([key, value]) => (
+                <MenuItem key={key} value={key}>{value}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+
+          <FormControl>
+            <InputLabel id="fontBackground-label">Background</InputLabel>
+            <Select
+              labelId="fontBackground-label"
+              id="fontBackground"
+              value={appSettings.subtitleSettings.backgroundHex}
+              label="Background"
+              onChange={(e) => handleBackground(e)}
+              size="small"
+            >
+              {Array.from(appSettings.subtitleSettings.backgrounds).map(([key, value]) => (
+                <MenuItem key={key} value={key}>{value}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+
+          <FormControl>
+            <Typography id="input-slider" gutterBottom sx={{ color: 'text.primary' }}>
+              Opacity
+            </Typography>
+            <Slider
+              aria-labelledby="opacity-slider"
+              value={appSettings.subtitleSettings.opacity}
+              onChange={(e, newValue) => handleOpacity(newValue)}
+              min={0} max={100} size="small"
+              valueLabelDisplay="auto"
+              slots={{
+                valueLabel: ValueLabelComponent,
+              }} />
+          </FormControl>
+
+          <FormControl>
+            <InputLabel id="lineSpacing-label">Line Spacing</InputLabel>
+            <Select
+              labelId="lineSpacing-label"
+              id="lineSpacing"
+              value={appSettings.subtitleSettings.lineSpacing}
+              label="Line Spacing"
+              onChange={handleLineSpacingChange}
+              size="small"
+            >
+              {Array.from(appSettings.subtitleSettings.lineSpacings).map(([key, value]) => (
+                <MenuItem key={key} value={key}>{value}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+
+         
+
+          <FormControl>
+            <InputLabel id="position-label">Position</InputLabel>
+            <Select
+              labelId="position-label"
+              id="position"
+              value={appSettings.subtitleSettings.position}
+              label="Position"
+              onChange={(e) => handlePosition(e)}
+              size="small"
+            >
+              <MenuItem value={"bottom"}>Bottom</MenuItem>
+              <MenuItem value={"top"}>Top</MenuItem>
+            </Select>
+          </FormControl>
+
+          <TextField
+            label="Profile Name"
+            variant="outlined"
+            fullWidth
+            value={appSettings.subtitleSettings.profileName}
+            sx={{ my: 2 }}
+            onChange={handleProfileNameChange}
+            size="small"
+          />
+          <Button
+            variant="contained"
+            onClick={handleSaveProfile}
+            size="small"
+          >
+            Save
+          </Button>
+        </Stack>
+
+        <Stack sx={{ padding: '20px' }}>
+          <Typography variant="h6" gutterBottom sx={{ color: 'text.primary' }}>
+            <strong>Profiles</strong>
+          </Typography>
+
+          <List dense={true} sx={{ color: 'text.primary' }}>
+            {Array.from(appSettings.subtitleSettings.profiles).map(([profileId, profile]) => (
+              <ListItem disableGutters key={profileId}
+                secondaryAction=  {profileId !== 0 ? <IconButton edge="end" onClick={() => handleDeleteProfile(profileId)}>
+                  <DeleteIcon key={profileId}/>
+                </IconButton> : ""}
+
+              >
+                <ListItemButton
+                  disableGutters
+                  selected={appSettings.subtitleSettings.currentProfile === profileId}
+                  onClick={() => handleProfileChange(profileId)}
+                >
+                  <ListItemText primary={profile.profileName} sx={{paddingLeft: '10px'}}/>
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+
+          <Typography variant="h6" gutterBottom sx={{ color: 'text.primary' }}>
+            <strong>Languages</strong>
+          </Typography>
+          <Box >
+            <FormControl sx={{ color: 'text.primary' }}>
+              <RadioGroup
+                value={appSettings.subtitleSettings.language}
+                onChange={handleLanguageChange}
+                size="small"
+              >
+                <FormControlLabel value="english" control={<Radio />} label="English" />
+                <FormControlLabel value="spanish" control={<Radio />} label="Spanish" />
+              </RadioGroup>
+            </FormControl>
+          </Box>
+
+          <Typography variant="h6" gutterBottom sx={{ color: 'text.primary' }}>
+            <strong>Switch On/Off</strong>
+          </Typography>
+          <Box >
+
+            <FormControl sx={{ color: 'text.primary' }}>
+              <RadioGroup
+                value={appSettings.subtitleSettings.switch}
+                onChange={handleSwitchChange}
+                size="small"
+              >
+                <FormControlLabel value="off" control={<Radio />} label="Off" />
+                <FormControlLabel value="on" control={<Radio />} label="On" />
+              </RadioGroup>
+            </FormControl>
+          </Box>
+        </Stack>
+      </Stack>
+    </div>
+  );
+}
